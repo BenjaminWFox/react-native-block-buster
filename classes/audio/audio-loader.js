65aa734af067b2b58eb7b5e61f91416a220ac01e
Ben Fox <benjamin.fox@smashingideas.com>
2019-03-22 11:53:34 -0700

    Add additional sounds. Allow repeat sounds without cuting audio

--- classes/audio/audio-loader.js
+++ classes/audio/audio-loader.js
@@ -1,5 +1,11 @@
 import { Audio } from 'expo'
 
+/**
+ * So expo doesn't let one sound play multiple times in sequence
+ * - it restarts and cuts off the previous play
+ *
+ * This class allows N repeat instances of a given sound before cutting off the previous
+ */
 class RepeatableSound {
   constructor(assetRequired, numRepeats) {
     this.assetRequired = assetRequired
@@ -10,9 +16,9 @@ class RepeatableSound {
   }
 
   init = async () => {
-    this.sounds = await Promise.all(this.sounds.map(() => Audio.Sound.createAsync(this.assetRequired)))
-
-    // console.log('Have our sounds', this.sounds)
+    this.sounds = await Promise.all(
+      this.sounds.map(() => Audio.Sound.createAsync(this.assetRequired)),
+    )
   }
 
   playAsync = () => {
@@ -24,14 +30,21 @@ class RepeatableSound {
   }
 }
 
-const c2 = require('../../assets/sounds/C2.mp3')
-const d2 = require('../../assets/sounds/D2.mp3')
-const e2 = require('../../assets/sounds/E2.mp3')
-const f2 = require('../../assets/sounds/F2.mp3')
-const g2 = require('../../assets/sounds/G2.mp3')
-const a3 = require('../../assets/sounds/A3.mp3')
-const b3 = require('../../assets/sounds/B3.mp3')
-const c3 = require('../../assets/sounds/C3.mp3')
+const c2 = require('../../assets/sounds/c2.mp3')
+const d2 = require('../../assets/sounds/d2.mp3')
+const e2 = require('../../assets/sounds/e2.mp3')
+const f2 = require('../../assets/sounds/f2.mp3')
+const g2 = require('../../assets/sounds/g2.mp3')
+const a3 = require('../../assets/sounds/a3.mp3')
+const b3 = require('../../assets/sounds/b3.mp3')
+const c3 = require('../../assets/sounds/c3.mp3')
+const d3 = require('../../assets/sounds/d3.mp3')
+const e3 = require('../../assets/sounds/e3.mp3')
+const f3 = require('../../assets/sounds/f3.mp3')
+const g3 = require('../../assets/sounds/g3.mp3')
+const a4 = require('../../assets/sounds/a4.mp3')
+const b4 = require('../../assets/sounds/b4.mp3')
+const c4 = require('../../assets/sounds/c4.mp3')
 
 const getAllSounds = () => [
   new RepeatableSound(c2, 5),
@@ -42,18 +55,14 @@ const getAllSounds = () => [
   new RepeatableSound(a3, 5),
   new RepeatableSound(b3, 5),
   new RepeatableSound(c3, 5),
+  new RepeatableSound(d3, 5),
+  new RepeatableSound(e3, 5),
+  new RepeatableSound(f3, 5),
+  new RepeatableSound(g3, 5),
+  new RepeatableSound(a4, 5),
+  new RepeatableSound(b4, 5),
+  new RepeatableSound(c4, 5),
 ]
-
-// const getAllSounds = async () => Promise.all([
-//   Audio.Sound.createAsync(require('../../assets/sounds/C2.mp3'), { shouldPlay: true, positionMillis: 100 }),
-//   Audio.Sound.createAsync(require('../../assets/sounds/D2.mp3'), { shouldPlay: true, positionMillis: 100 }),
-//   Audio.Sound.createAsync(require('../../assets/sounds/E2.mp3'), { shouldPlay: true, positionMillis: 100 }),
-//   Audio.Sound.createAsync(require('../../assets/sounds/F2.mp3'), { shouldPlay: true, positionMillis: 100 }),
-//   Audio.Sound.createAsync(require('../../assets/sounds/G2.mp3'), { shouldPlay: true, positionMillis: 100 }),
-//   Audio.Sound.createAsync(require('../../assets/sounds/A3.mp3'), { shouldPlay: true, positionMillis: 100 }),
-//   Audio.Sound.createAsync(require('../../assets/sounds/B3.mp3'), { shouldPlay: true, positionMillis: 100 }),
-//   Audio.Sound.createAsync(require('../../assets/sounds/C3.mp3'), { shouldPlay: true, positionMillis: 100 }),
-// ])
 
 class AudioLoader {
   constructor() {
